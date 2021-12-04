a482e1f08db108ad1e783688b5d6dab37dd1f246
Sem <12117498+egoshift@users.noreply.github.com>
2021-12-04 23:59:32 +0800

    [Feat][Ems] setup nest

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { RouterModule } from '@nestjs/core';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AnimeModule } from './anime/anime.module';
+import { WikiModule } from './wiki/wiki.module';
+import Routes from './routes/'
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot(), RouterModule.register(Routes), AnimeModule, WikiModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
+
+import * as admin from 'firebase-admin';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService: ConfigService = app.get(ConfigService)
+
+  admin.initializeApp({
+    credential: admin.credential.cert({
+      projectId: configService.get<string>('FIREBASE_PROJECT_ID'),
+      privateKey: configService.get<string>('FIREBASE_PRIVATE_KEY').replace(/\\n/g, '\n'),
+      clientEmail: configService.get<string>('FIREBASE_CLIENT_EMAIL'),
+    }),
+    databaseURL: 'https://anijouhou.firebaseio.com'
+  })
+
+  app.enableCors()
+  
   await app.listen(3000);
 }
 bootstrap();
