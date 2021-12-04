7924efb31f91282242d215df9ebbfcf64d1f57ad
Sem <12117498+egoshift@users.noreply.github.com>
2021-12-05 00:00:55 +0800

    [Feat][Ems] added constants

---
+++ src/constants/sources.ts
@@ -0,0 +1,13 @@
+const ENDPOINT = {
+  API: 'https://cdn.animenewsnetwork.com/encyclopedia/api.xml',
+  REPORTS: 'https://www.animenewsnetwork.com/encyclopedia/reports.xml',
+}
+
+const REPORTS = {
+  LIST_REPORT_ID: 155,
+  LIST_REPORT_ALL: 'all',
+}
+
+export const SOURCES = {
+  ENDPOINT, REPORTS,
+}
